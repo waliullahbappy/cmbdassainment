ef352f56dc352d7743b21f40c74c0a3606efc9b6
waliullahbappy <75230047+waliullahbappy@users.noreply.github.com>
2022-03-24 16:00:05 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,124 @@
+
+
+//  PROBLEM 1 : Solve the problem with Javascript  to find the maximum between two numbers.
+
+// let numberOne = 11;
+// let NumberTwo = 10
+// if (numberOne > NumberTwo){
+//     console.log(" Number One Is The Maximum Number");
+// }else{
+//     console.log(" Number Two Is The Maximum Number");
+// }
+
+
+//  PROBLEM  2:Solve the problem with Javascript  to find a maximum between three numbers.
+
+// var numberOne = 3;
+// var numberTwo = 5;
+// var numberThree = 12
+
+// if (numberOne > numberTwo && numberOne > numberThree){
+//     console.log(" Number One Is The Maximum Number");
+// }
+// else if(numberTwo > numberOne && numberTwo > numberThree){
+//     console.log(" Number Two Is The Maximum Number");
+// }else{
+//     console.log(" Number Three Is The Maximum Number");
+// }
+
+// PROBLEM 3 : Solve the problem with Javascript  to check whether a number is negative, positive or zero.
+
+// const number = -1
+// if (number > 0) {
+//     console.log("The number is positive");
+// }
+
+// else if (number == 0) {
+//   console.log("The number is zero");
+// }
+// else {
+//      console.log("The number is negative");
+// }
+
+
+// PROBLEM 4 :Solve the problem with Javascript  to check whether a number is divisible by 40 or not.
+
+// var a = 4; 
+// var b = 40;
+
+// if ((a % b) == 0) {
+//     console.log("This Number is divisible by 40");
+// }
+// else {
+//     console.log("This Number is not divisible by 40");
+// }
+
+
+// PROBLEM 5 : Solve the problem with Javascript  to check whether a number is even or odd.
+
+// var x = 5; 
+
+// if (( x % 2) == 0) {
+//     console.log("This Number is Even Number");
+// }
+// else {
+//     console.log("This Number is Odd Number");
+// }
+
+
+// PROBLEM 6 :Solve the problem with Javascript  to check whether a character is in the alphabet or not.
+
+//  var character ="Z"
+
+//  if(character>= 'A' && character<= 'Z'){
+//     console.log("This Is Alphabet Number");
+//  }else{
+//     console.log("This Is Not Alphabet Number");
+//  }
+
+
+//  PROBLEM 7 :Solve the problem with Javascript  to check weather the temperature is hot, cold or normal
+// var temparature = 1
+
+// if (temparature <= 8){
+//    console.log('Temparature Is Cold');
+// }else if (temparature > 8 && temparature <= 25){
+//    console.log('Temparature Is Normal');
+// }else{
+// console.log('Temparature is Hot')
+// }
+
+// PROBLEM 8 : Given 3 subjects Math, English and Physics find the total marks and average of 3 subjects.
+// Find out if the average grade:
+// If Percentage >= 90% then print “Grade A”
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+
+
+// var english = 90;
+// var math = 60;
+// var Physics = 50;
+
+// var totalMarks = english + math + Physics
+// var avarage = totalMarks/3
+// if (avarage >= 90){
+//    console.log('Grade A');
+// }else if(avarage >= 80){
+//    console.log('Grade B');
+// }else if(avarage >= 70){
+//    console.log('Grade C');
+// }else if(avarage >= 60){
+//    console.log('Grade D');
+// }else if(avarage >= 40){
+//    console.log('Grade E');
+// }else if(avarage < 40){
+//    console.log('Grade F');
+// }
+// console.log('Avarage Marks : '+avarage);
+// console.log('Total Marks : ' + totalMarks);
+
+
+
